e20ab5b67fc3e41f44d33b53e581b28b162b23ba
Evan Suma Rosenberg <suma@umn.edu>
2022-01-21 13:39:51 -0600

    finished coding example

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,12 @@ class Game
     private width : number;
     private height : number;
     private parentNode : paper.Group | undefined;
-    private childNode : paper.Group | undefined;
+    private childNode1 : paper.Group | undefined;
+    private childNode2 : paper.Group | undefined;
     private selectedNode : paper.Item | undefined;
     private lastMousePoint : paper.Point;
-    private text : paper.PointText | undefined;
+    private text1 : paper.PointText | undefined;
+    private text2 : paper.PointText | undefined;
     
     constructor()
     {
@@ -36,47 +38,83 @@ class Game
         paper.view.onMouseMove = (event: paper.MouseEvent) => {this.onMouseMove(event)};
         paper.view.onMouseDown = (event: paper.MouseEvent) => {this.onMouseDown(event)};
         paper.view.onFrame = (event: GameEvent) => {this.update(event)};
+        new paper.Tool().onKeyDown = (event: paper.KeyEvent) => {this.onKeyDown(event)};
     }
 
     private createScene() : void 
     {
-        // create a parent and child screne graph node
+        // create a parent and two child screne graph nodes
         this.parentNode = new paper.Group();
-        this.childNode = new paper.Group();
-        this.childNode.addTo(this.parentNode);
+        this.childNode1 = new paper.Group();
+        this.childNode2 = new paper.Group();
+        this.childNode1.addTo(this.parentNode);
+        this.childNode2.addTo(this.parentNode);
 
         // create the rectangle geometry
         var rectGeometry = new paper.Rectangle(new paper.Point(0, 0), new paper.Size(50, 50));
-        var drawableRect = new paper.Path.Rectangle(rectGeometry);
-        drawableRect.fillColor = new paper.Color('purple');
+
+        // create a purple drawable rectangle from the geometry and add it to the parent node
+        var redDrawableRect = new paper.Path.Rectangle(rectGeometry);
+        redDrawableRect.fillColor = new paper.Color('red');
+        redDrawableRect.position = new paper.Point(0, 0);
+        redDrawableRect.addTo(this.parentNode);
+        
+        // create a purple drawable rectangle from the geometry
+        var purpleDrawableRect = new paper.Path.Rectangle(rectGeometry);
+        purpleDrawableRect.fillColor = new paper.Color('purple');
 
         // create a symbol from the rectangle geometry
-        var rectSymbol = new paper.SymbolDefinition(drawableRect);
+        var rectSymbol = new paper.SymbolDefinition(purpleDrawableRect);
 
         // create two instances of the rectangle
-        var rectInstance1 = rectSymbol.place(new paper.Point(0, 0));
-        var rectInstance2 = rectSymbol.place(new paper.Point(200, 0));
+        var rectInstance1 = rectSymbol.place(new paper.Point(200, 0));
+        var rectInstance2 = rectSymbol.place(new paper.Point(0, 200));
 
-        // add one instance to the parent node and one instance to the child node
-        rectInstance1.addTo(this.parentNode);
-        rectInstance2.addTo(this.childNode);
+        // add both instances to the child node
+        rectInstance1.addTo(this.childNode1);
+        rectInstance2.addTo(this.childNode2);
 
         // perform an initial translation of the parent node
         this.parentNode.translate(new paper.Point(200, 200));
 
         // create some text and place it at the center of the bottom of the view
-        this.text = new paper.PointText(new paper.Point(0, 0));
-        this.text.fontSize = 30;
-        this.text.content = 'Press r to rotate.  Press s to scale.';
-        this.text.justification = 'center';
-        this.text.position = new paper.Point(paper.view.center.x - 50, paper.view.size.height - 50);
-        this.text.visible = true;
+        this.text1 = new paper.PointText(new paper.Point(0, 0));
+        this.text1.fontSize = 24;
+        this.text1.content = 'Click the red box to select the parent node. Click the purple box to select the child node.';
+        this.text1.justification = 'center';
+        this.text1.position = new paper.Point(paper.view.center.x - 50, paper.view.size.height - 50);
+
+        // create some text and place it at the center of the bottom of the view
+        this.text2 = new paper.PointText(new paper.Point(0, 0));
+        this.text2.fontSize = 30;
+        this.text2.content = 'Press r to rotate.  Press s to scale.';
+        this.text2.justification = 'center';
+        this.text2.position = new paper.Point(paper.view.center.x - 50, paper.view.size.height - 50);
+        this.text2.visible = false;
     }
 
     // This method will be called once per frame
     private update(event: GameEvent) : void
     {
         
+    }
+
+    private onKeyDown(event: paper.KeyEvent) : void
+    {
+        if(this.selectedNode)
+        {
+            if(event.key == 'r')
+            {
+                // rotate the node by 10 degrees
+                this.selectedNode.rotate(10, this.lastMousePoint);
+            }
+            else if(event.key == 's')
+            {
+                // increase the node scale by 1%
+                this.selectedNode.scale(1.01, this.lastMousePoint);
+            }
+
+        }
     }
 
     private onMouseMove(event: paper.MouseEvent) : void
@@ -94,7 +132,8 @@ class Game
         if(this.selectedNode)
         {
             this.selectedNode = undefined;
-            this.text!.visible = false;
+            this.text1!.visible = true;
+            this.text2!.visible = false;
         }
         // if we have not selected a node, conduct a hit test
         else
@@ -104,7 +143,12 @@ class Game
 
             // get the group node, which is the parent of the drawable item
             this.selectedNode = hits.at(0)?.item.parent;
-            this.text!.visible = true;
+
+            if(this.selectedNode)
+            {
+                this.text1!.visible = false;
+                this.text2!.visible = true;
+            }
         }
     }  
 
